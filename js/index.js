dc4299e63cde3c00309aa2b185b61fd2f3303a8d
Bert Heyman <bert.heyman@hotmail.com>
2023-03-08 11:46:57 +0100

    Class function syntax instead of fat arrow
    Mixture could be very confusing. Current guideline for students is to opt for classic style syntax.

--- js/index.js
+++ js/index.js
@@ -22,9 +22,9 @@ const wordl = {
     },
     pickRandomWord() {
     },
-    matchWord: () => {
+    matchWord() {
     },
-    addGuess: () => {
+    addGuess() {
     },
     showSolution() {
     },
